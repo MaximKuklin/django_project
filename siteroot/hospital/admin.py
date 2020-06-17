7a37cb90cb442b4b25ec16546f5df2b7488d7451
Maxim <maxim.kuklin@xperience.ai>
2020-06-18 01:03:23 +0300

    add admin functions

--- siteroot/hospital/admin.py
+++ siteroot/hospital/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from django.db.models import Max, F
+from .models import *
+
+
+class RecordInline(admin.TabularInline):
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    model = Record
+
+    fields = ('condition', 'text', 'created_at', 'is_modified')
+    readonly_fields = ('condition', 'text', 'created_at', 'is_modified')
+    ordering = ('-created_at',)
+    show_change_link = True
+
+
+class RecordAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(doctor=F('sick_list__doctor__username'),
+                                                      title=F('sick_list__title'))
+
+    def doctor(self, obj):
+        return obj.doctor
+
+    def title(self, obj):
+        return obj.title
+
+
+    list_display = ('doctor', 'title', 'condition', 'text', 'created_at', 'is_modified')
+    ordering = ('-created_at',)
+    list_display_links = ('title',)
+    fields = ('sick_list', 'title', 'condition', 'text', 'created_at', 'updated_at')
+    readonly_fields = ('sick_list', 'created_at', 'updated_at')
+    doctor.admin_order_field = 'doctor'
+
+
+
+class SickListAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        return False
+
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(updated_at=Max('created_at'))
+
+    def updated_at(self, obj):
+        return obj.updated_at
+
+    list_display = ('title', 'doctor', 'created_at')
+    ordering = ('title',)
+    fields = ('title', 'doctor', 'created_at')
+    readonly_fields = ('created_at',)
+    view_on_site = True
+    inlines = (RecordInline,)
+
+
+admin.site.register(SickList, SickListAdmin)
+admin.site.register(Record, RecordAdmin)
 
 # Register your models here.
--- siteroot/hospital/models.py
+++ siteroot/hospital/models.py
@@ -1,13 +1,19 @@
 from django.db.models import *
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 class SickList(Model):
     doctor = ForeignKey(User, on_delete=CASCADE)
     title = CharField(max_length=200)
     created_at = DateTimeField(auto_now_add=True)
 
+    def get_absolute_url(self):
+        return reverse('sicklist_by_id', kwargs={'sicklist_id': self.id})
+
     def __str__(self):
-        return str(self.title)
+        return f"{self.title}, doctor: {self.doctor.username}"
+
 
 class Record(Model):
     sick_list = ForeignKey(SickList, on_delete=CASCADE)
@@ -16,5 +22,13 @@ class Record(Model):
     text = TextField(max_length=4096)
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
+
+    def is_modified(self):
+        return (self.created_at - self.updated_at).total_seconds() > 0
+
+    is_modified.boolean = True
+
+    def __str__(self):
+        return f"id: {self.id} sicklist: {self.sick_list.id}"
 
 # Create your models here.
